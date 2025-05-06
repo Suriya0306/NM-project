aaf88b23906eb22fba684893b624b634c7c958fc
Suriya0306 <nsuriya310@gmail.com>
2025-05-07 00:27:58 +0530

    Create emotion_mapper.py

---
+++ emotion_mapper.py
@@ -0,0 +1,15 @@
+# Emoji Mapping for Sentiments
+emoji_map = {
+    "Positive": "😊",
+    "Negative": "😠",
+    "Neutral": "😐"
+}
+
+# Emotion Mapping
+emotion_map = {
+    "joy": "😊",
+    "sadness": "😢",
+    "anger": "😠",
+    "fear": "😨",
+    "surprise": "😮"
+}
